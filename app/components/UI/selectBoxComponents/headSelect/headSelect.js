72d91cebf9e1967fa536d05253dfb27dbfd2c5df
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-09-29 13:31:35 -0700

    Priming headSelect

---
+++ app/components/UI/selectBoxComponents/headSelect/headSelect.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Style from "./headSelect.scss";
+
+export default class HeadSelect extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		return (
+			<span className='headSelect'>
+			</span>
+		);
+	}
+}
