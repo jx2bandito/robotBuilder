ab97afe075efa35d331326aae9149f24347a061d
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-09-30 03:25:06 -0700

    Put in placeholder opening/closing animation

--- app/components/UI/selectBoxComponents/headSelect/headSelect.js
+++ app/components/UI/selectBoxComponents/headSelect/headSelect.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Style from "./headSelect.scss";
-
+import gsap from "gsap";
 export default class HeadSelect extends React.Component{
 	constructor(props){
 		super(props);
+		
+		this.state = {open:true};
+		this.handleClick = this.handleClick.bind(this);
+	}
+	
+	handleClick(){
+		if (!this.state.open){
+			this.setState({open: true});
+			TweenMax.to(".headSelect", .5, {width: "100%"});
+		}
+		else if (this.state.open){
+			this.setState({open: false});
+			TweenMax.to(".headSelect", .5, {width: "20%"});
+		}
 	}
 	
 	render(){
 		return (
-			<span className='headSelect'>
+			<span className='headSelect' onClick={this.handleClick}>
 			</span>
 		);
 	}
-}
+}	
