b83485d3c0a81ce2c221710a43fd8d356d6aa812
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-10-02 08:18:23 -0700

    Added <HeadSelect /> and event handlers

--- app/components/UI/selectBoxComponents/selectBox.js
+++ app/components/UI/selectBoxComponents/selectBox.js
@@ -1,45 +1,42 @@
 import React from 'react';
 import Style from './selectBoxStyleCompiled.scss';
-import HeadSelect from "./headSelect/headSelect.js";
+import HeadSelect from "./headSelect.js";
 
 
 export default class SelectBox extends React.Component{
 	constructor(props){
 		super(props);
+		
 		this.state = {
-			addTabClass1: null
+			addTabClass1: null,
+			addTabClass2: null,
+			addTabClass3: null,
+			addTabClass4: null
 		}
 		
-		this.handleMouseEnter1 = this.handleMouseEnter1.bind(this);
-		this.handleMouseLeave1 = this.handleMouseLeave1.bind(this);
+		this.toggleOn = this.toggleOn.bind(this);
+		this.toggleOff = this.toggleOff.bind(this);
 	}
 	
-	handleMouseEnter1(param){
-		alert(param)
-		this.setState({addTabClass1: "toggledOn"});
+	toggleOn(tabNum){ ////Takes an integer indicating which tab to affe
+		var newKey = "addTabClass" + tabNum
+		var newState = {};
+		newState[newKey] = "toggledOn";
+		this.setState(newState);
 	}
 	
-	handleMouseLeave1(){
-		this.setState({addTabClass1: null});
+	toggleOff(tabNum){////Takes an integer indicating which tab to affect
+		var newKey = "addTabClass" + tabNum
+		var newState = {};
+		newState[newKey] = null;
+		this.setState(newState);
 	}
 	
 	render(){
 		return (
 			<div className='selectBoxContainer'>
-			
-				<span className={"tab " + this.state.addTabClass1} onMouseEnter={this.handleMouseEnter1} onMouseLeave={this.handleMouseLeave1}>
-					<p>Head</p>
-					<span className="preview">
-						<span className="portrait"></span>
-						<span className="description">
-							Kabuto helmet <br/>
-							Demoralize enemies <br/>
-							Behind it, third eye
-						</span>
-					</span>
-					<i className="fa fa-toggle-right fa-lg"></i>
-					<i className="fa fa-toggle-left fa-lg"></i>
-				</span>
+				
+				<HeadSelect handleMouseEnter={this.toggleOn} handleMouseLeave={this.toggleOff} addTabClass1={this.state.addTabClass1} />
 				
 				<span className="tab">
 					<p>Armor</p>
