7f764b10d2f1b862861caae24a57bd254735b284
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-10-05 11:14:32 -0700

    Primed to be able to dynamically render different parts

--- app/components/app.js
+++ app/components/app.js
@@ -1,28 +1,48 @@
 import React from 'react';
-import SamuraiHead from './robotHeads/samurai/samuraiHead.js';
 import SelectBox from "./UI/selectBoxComponents/selectBox.js";
+import {headList, bodyList, armsList, legsList} from "./partsList.js";
 
 
-const HeadList = {
-	samuraiHead: {
-		display: SamuraiHead
-	}
-}
 export default class App extends React.Component{
+
 	constructor(props){
 		super(props);
 		this.state = {
-			currentHead: "samuraiHead"
+			currentHead: "none",
+			currentBody: "none",
+			currentArms: "none",
+			currentLegs: "none"
 		};
+		
+		this.changePart=this.changePart.bind(this);
+	}
+	
+	changePart(partType, partName){
+		if(!/(currentHead|currentBody|currentArms|currentLegs)/.test(partType)){
+			//Limit power of changePart
+			console.log("Cannot use changePart in app.js to change unrelated states");
+			return false;
+		}
+		
+		this.setState({[partType]: partName})
 	}
 	
 	render(){
-		let Head = HeadList[this.state.currentHead].display
+		let Head = headList[this.state.currentHead].display; 
+		let Body = bodyList[this.state.currentBody].display; 
+		let Arms = armsList[this.state.currentArms].display; 
+		let Legs = legsList[this.state.currentLegs].display; 
+		
 		return (
 			<div>
-				<SelectBox />
-				<Head />
+				<SelectBox changePart={this.changePart}/>
+				{Head ? <Head addClass="jumbo" /> : false}
+				{Body ? <Body /> : false}
+				{Arms ? <Arms /> : false}
+				{Legs ? <Legs /> : false}
+				{/*Render parts if they exist*/}
 			</div>
 		)
+		
 	}
 }
