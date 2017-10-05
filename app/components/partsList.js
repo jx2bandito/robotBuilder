05059172b3076d2ed9f932f351fa1b9fa310103a
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-10-05 11:14:57 -0700

    object where list of parts will be kept

---
+++ app/components/partsList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import SamuraiHead from './robotHeads/samurai/samuraiHead.js';
+
+export const headList = {
+	samuraiHead: {
+		display: SamuraiHead,
+		description: (<span>
+			Kabuto helmet <br />
+			Demoralize enemies <br />
+			Behind it, third eye
+			</span>)
+	},
+	none: {
+		display: false
+	}
+}
+
+export const bodyList = {
+	none: {
+		display: false
+	}
+}
+
+export const armsList = {
+	none: {
+		display: false
+	}
+}
+
+export const legsList = {
+	none: {
+		display: false
+	}
+}
