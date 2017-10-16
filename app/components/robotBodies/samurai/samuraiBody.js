224718f6e939f7a7b7ac61c4b1a915ad264233af
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-10-16 16:26:57 -0700

    Redesigned samuraiBody

--- app/components/robotBodies/samurai/samuraiBody.js
+++ app/components/robotBodies/samurai/samuraiBody.js
@@ -14,82 +14,82 @@ export default class samuraiBody extends React.Component{
 			return false;
 		}
 		
-	var samuraiBodyTL = new TimelineMax();
+		var samuraiBodyTL = new TimelineMax();
 
-	samuraiBodyTL
-	  .from(".jumbo.samuraiBody", 1, {
-		y: "100%"
-	  })
-	  .to(
-		".jumbo.coreCoverLeft",
-		1,
-		{
-		  width: "0",
-		  right: "50%"
-		},
-		"coverMove"
-	  )
-	  .to(
-		".jumbo.coreCoverRight",
-		1,
-		{
-		  width: "0",
-		  onStart: function() {
-			document.getElementsByClassName("core")[1].className += " animated";
-		  }
-		},
-		"coverMove"
-	  )
-	  .from(".jumbo.backPlate.plate1", 1, {
-		y: "10",
-		scale: 0.8,
-		display: "none",
-		delay: 0.25,
-		ease: Power4.easeInOut
-	  })
-	  .from(".jumbo.backPlate.plate2", 1, {
-		y: "-60%",
-		display: "none",
-		ease: Power4.easeInOut
-	  }, "-=.25")
-	  .from(".jumbo.frontPlate.plate1", 1, {
-		y: "-10",
-		display: "none",
-		scale: 1.11,
-		delay: 0.25,
-		ease: Power4.easeInOut
-	  })
-	  .from(".jumbo.frontPlate.plate2", 1, { y: "-60%", display: "none", ease: Power4.easeInOut }, "-=.25")
-	  .from(
-		".padLeft",
-		1,
-		{
-		  x: "35%",
-		  ease: Power4.easeInOut 
-		},
-		"padMove"
-	  )
-	  .from(
-		".jumbo.padRight",
-		1,
-		{
-		  x: "-35%",
-		  ease: Power4.easeInOut 
-		},
-		"padMove"
-	  )
-	  .from(".jumbo.penta", 1, {
-		rotationX: "90deg",
-		ease: Back.easeOut
-	  })
-	  .from(".jumbo.bodyAnimWrap", .5, {
-		y: "5%",
-		ease: Back.easeInOut
-	  })
-	  .from(".jumbo.neck", .5, {
-		scale: "0"
-	  }, "-=.15")
-	  .timeScale(2.5);
+		samuraiBodyTL
+		  .from(".jumbo.samuraiBody", 1, {
+			y: "100%"
+		  })
+		  .to(
+			".jumbo.coreCoverLeft",
+			1,
+			{
+			  width: "0",
+			  right: "50%"
+			},
+			"coverMove"
+		  )
+		  .to(
+			".jumbo.coreCoverRight",
+			1,
+			{
+			  width: "0",
+			  onStart: function() {
+				document.getElementsByClassName("core")[1].className += " animated";
+			  }
+			},
+			"coverMove"
+		  )
+		  .from(".jumbo.backPlate.plate1", 1, {
+			y: "10",
+			scale: 0.8,
+			display: "none",
+			delay: 0.25,
+			ease: Power4.easeInOut
+		  })
+		  .from(".jumbo.backPlate.plate2", 1, {
+			y: "-60%",
+			display: "none",
+			ease: Power4.easeInOut
+		  }, "-=.25")
+		  .from(".jumbo.frontPlate.plate1", 1, {
+			y: "-10",
+			display: "none",
+			scale: 1.11,
+			delay: 0.25,
+			ease: Power4.easeInOut
+		  })
+		  .from(".jumbo.frontPlate.plate2", 1, { y: "-60%", display: "none", ease: Power4.easeInOut }, "-=.25")
+		  .from(
+			".padLeft",
+			1,
+			{
+			  x: "35%",
+			  ease: Power4.easeInOut 
+			},
+			"padMove"
+		  )
+		  .from(
+			".jumbo.padRight",
+			1,
+			{
+			  x: "-35%",
+			  ease: Power4.easeInOut 
+			},
+			"padMove"
+		  )
+		  .from(".jumbo.penta", 1, {
+			rotationX: "90deg",
+			ease: Back.easeOut
+		  })
+		  .from(".jumbo.bodyAnimWrap", .5, {
+			y: "5%",
+			ease: Back.easeInOut
+		  }) 
+		  .from(".jumbo.neck", .5, {
+			scale: "0"
+		  }, "-=.15")
+		  .timeScale(2.5);
 		
 	}
 	
