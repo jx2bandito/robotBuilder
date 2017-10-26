256a35c5b0db3b97110b9915cbd8036138536fe3
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-10-25 18:54:57 -0700

    No longer takes mini and jumbo classes

--- app/components/robotBodies/samurai/samuraiBody.js
+++ app/components/robotBodies/samurai/samuraiBody.js
@@ -9,122 +9,129 @@ export default class samuraiBody extends React.Component{
 	}
 	
 	componentDidMount(){
-		let speed = 2;
-		if(this.props.addClass == "mini"){
+		let speed = 2.2;
+		
+		if(!document.querySelector(".animationWrapper .samuraiBody")){
 			return false;
 		}
 		
-		var samuraiBodyTL = new TimelineMax();
-
-		samuraiBodyTL
-		  .from(".jumbo.samuraiBody", 1, {
-			y: "100%"
-		  })
-		  .to(
-			".jumbo.coreCoverLeft",
-			1,
-			{
-			  width: "0",
-			  right: "50%"
-			},
-			"coverMove"
-		  )
-		  .to(
-			".jumbo.coreCoverRight",
-			1,
-			{
-			  width: "0",
-			  onStart: function() {
-				document.getElementsByClassName("core")[1].className += " animated";
-			  }
-			},
-			"coverMove"
-		  )
-		  .from(".jumbo.backPlate.plate1", 1, {
-			y: "10",
-			scale: 0.8,
-			display: "none",
-			delay: 0.25,
-			ease: Power4.easeInOut
-		  })
-		  .from(".jumbo.backPlate.plate2", 1, {
-			y: "-60%",
-			display: "none",
-			ease: Power4.easeInOut
-		  }, "-=.25")
-		  .from(".jumbo.frontPlate.plate1", 1, {
-			y: "-10",
-			display: "none",
-			scale: 1.11,
-			delay: 0.25,
-			ease: Power4.easeInOut
-		  })
-		  .from(".jumbo.frontPlate.plate2", 1, { y: "-60%", display: "none", ease: Power4.easeInOut }, "-=.25")
-		  .from(
-			".padLeft",
-			1,
-			{
-			  x: "35%",
-			  ease: Power4.easeInOut 
-			},
-			"padMove"
-		  )
-		  .from(
-			".jumbo.padRight",
-			1,
-			{
-			  x: "-35%",
-			  ease: Power4.easeInOut 
-			},
-			"padMove"
-		  )
-		  .from(".jumbo.penta", 1, {
-			rotationX: "90deg",
-			ease: Back.easeOut
-		  })
-		  .from(".jumbo.bodyAnimWrap", .5, {
-			y: "5%",
-			ease: Back.easeInOut
-		  }) 
-		  .from(".jumbo.neck", .5, {
-			scale: "0"
-		  }, "-=.15")
-		  .timeScale(2.5);
 		
+		var samuraiBodyTL = new TimelineMax();
+			samuraiBodyTL
+			  .from(".animationWrapper .samuraiBody", 1, {
+				scale: 1.05,
+				display: "none"
+			  })
+			  .from(".animationWrapper .samuraiLowerBody", 1, {
+				rotationX: "-90deg",
+				display: "none"
+			  })
+			  .to(".animationWrapper .coreCoverLeft", 0.5, {
+				  width: "0%",
+				  right: "50%"
+				},
+				"coverMove"
+			  )
+			  .to(".animationWrapper .coreCoverRight", 0.5, {
+				  width: "0%",
+				  onStart: function() {
+					document.querySelector(".animationWrapper .core").className += " animated";
+				  }
+				},
+				"coverMove"
+			  )
+			  .from(".animationWrapper .backPlate.plate1", 1, {
+				scale: 0.95,
+				display: "none",
+				onStart: function() {
+					var legAnim = document.querySelector(".animationWrapper .legAnimWrap");
+					if(legAnim){
+						legAnim.style.zIndex = 301;
+					}
+				  document.querySelector(".animationWrapper .armContainerLeft").style.zIndex = 301;
+				  document.querySelector(".animationWrapper .armContainerRight").style.zIndex = 301;
+				},
+				delay: 0.25,
+				ease: Power4.easeInOut
+			  })
+			  .from(".animationWrapper .backPlate.plate2", 1, {
+				  y: "-60%",
+				  display: "none",
+				  ease: Power4.easeInOut
+				},
+				"-=.25"
+			  )
+			  .from(".animationWrapper  .frontPlate.plate1", 1, {
+				y: "-0.25%",
+				onStart: function() {
+				  document.querySelector(".animationWrapper .armContainerLeft").style.zIndex = 298;
+				  document.querySelector(".animationWrapper .armContainerRight").style.zIndex = 298;
+				},
+				display: "none",
+				scale: 1.11,
+				delay: 0.25,
+				ease: Power4.easeInOut
+			  })
+			  .from(".animationWrapper .frontPlate.plate2", 1, {
+				  y: "-60%",
+				  display: "none",
+				  ease: Power4.easeInOut,
+				  onStart: function() {
+					document.querySelector(".animationWrapper .legAnimWrap").style.zIndex = 298;
+				  }
+				},
+				"-=.25"
+			  )
+			  .from(".animationWrapper .padLeft", 1, {
+				  x: "40%",
+				  ease: Power4.easeInOut
+				},
+				"padMove"
+			  )
+			  .from(".animationWrapper .padRight", 1, {
+				  x: "-40%",
+				  ease: Power4.easeInOut
+				},
+				"padMove"
+			  )
+			  .from(".animationWrapper .penta", 1, {
+				rotationX: "90deg",
+				ease: Back.easeOut
+			  })
+			  .from(".animationWrapper .neck", 0.5, {
+				  scale: "0"
+				},
+				"-=.15"
+			  )
+			  .timeScale(speed)
+			  .play(0);
 	}
 	
 	render(){
 		return (
-			<div className={"bodyAnimWrap " + this.props.addClass}>
-				<div className={"samuraiBodyWrapper " + this.props.addClass}>
-					<span className={"backPlate plate1 " + this.props.addClass}>
-						<span className={"neck " + this.props.addClass}></span>
-					</span>
-					<span className={"backPlate plate2 " + this.props.addClass}></span>
-					<div className={"samuraiBody " + this.props.addClass}>
-					<span className={"core " + this.props.addClass}>
-						<span className={"coreCoverLeft coreCover " + this.props.addClass}></span>
-						<span className={"coreCoverRight coreCover " + this.props.addClass}></span>
-						<span className={"coreMiddle " + this.props.addClass}></span>
-						<span className={"coreEnd " + this.props.addClass}></span>
-					</span>
-					</div>
-				<span className={"frontPlate plate1 " + this.props.addClass}>
-					<span className={"neck " + this.props.addClass}></span>
-					<span className={"pad padLeft " + this.props.addClass}></span>
-					<span className={"pad padRight " + this.props.addClass}></span>
-					<span className={"penta " + this.props.addClass}>
-						<img className={"shi " + this.props.addClass} 
-						src={death} 
-						alt="Japanese character for death"/>
-					</span>
+			<div className="bodyAnimWrap">
+			  <div className="samuraiBodyWrapper">
+				<span className="backPlate plate1">
+				  <span className="neck"></span>
 				</span>
-				<span className={"frontPlate plate2 " + this.props.addClass}>
-					<span className={"belt " + this.props.addClass}></span>
+				<span className="backPlate plate2"></span>
+				<span className="samuraiBody">
+				  <span className="core"><span className="coreMiddle"></span><span className="coreEnd"></span></span>
+				<span className="coreCoverLeft coreCover"></span>
+				<span className="coreCoverRight coreCover"></span>
+
 				</span>
-				</div>
-			</div>
-				
+				<span className="samuraiLowerBody"></span>
+					<span className="frontPlate plate1">
+				  <span className="pad padLeft"></span>
+				<span className="pad padRight"></span>
+				<span className="penta"><img className="shi" src={death} alt="Japanese character for death"/></span>
+				</span>
+				<span className="frontPlate plate2">
+				  <span className="belt"></span>
+				</span>
+			  </div>
+			</div>		
 		);
 	}
 }
