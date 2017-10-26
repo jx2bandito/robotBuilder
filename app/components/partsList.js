41788af0050734751c4868dc3d2d954fc57cc73b
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-10-25 18:54:26 -0700

    added samuraiLegs and samuraiArms

--- app/components/partsList.js
+++ app/components/partsList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SamuraiHead from './robotHeads/samurai/samuraiHead.js';
 import SamuraiBody from "./robotBodies/samurai/samuraiBody.js";
+import SamuraiArms from "./robotArms/samurai/samuraiArms.js";
+import SamuraiLegs from "./robotLegs/samurai/samuraiLegs.js";
 import Placeholder from "./placeholder.js";
 
 
@@ -35,6 +37,14 @@ export const bodyList = {
 }
 
 export const armsList = {
+	samuraiArms: {
+		display: SamuraiArms,
+		description: (<span>
+			Dual katanas <br />
+			Miyamoto Musashi<br />
+			Would be proud indeed
+			</span>)
+	},
 	placeholder: {
 		display: Placeholder,
 		description: (<span>ERROR! Insufficient funds</span>)
@@ -42,6 +52,14 @@ export const armsList = {
 }
 
 export const legsList = {
+	samuraiLegs: {
+		display: SamuraiLegs,
+		description: (<span>
+			Quick as a swallow<br />
+			Running through the battlefield<br />
+			Leaving death behind
+			</span>)
+	},
 	placeholder: {
 		display: Placeholder,
 		description: (<span>ERROR! Not yet authorized to build this part</span>)
